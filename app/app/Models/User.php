e798fa6f86f3919b08b1843034991a6558988a66
Paul-Ben Ajene <paulben.ajene@gmail.com>
2024-01-13 16:35:37 +0100

    user resource adeed to admin dashboard

--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -3,15 +3,52 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    const ADMIN_ROLE = 1;
+    const SUPPORT = 2;
+    const STAFF_ROLE = 3;
+    const STUDENT_ROLE = 4;
+
+    const MALE = 'MALE';
+    const FEMALE = 'FEMALE';
+
+    const GENDER = [
+        self::MALE => "Male",
+        self::FEMALE => 'Female'
+    ];
+
+    const ROLES = [
+        self::ADMIN_ROLE => 'Admin',
+        self::SUPPORT => 'Support',
+        self::STAFF_ROLE => 'Staff',
+        self::STUDENT_ROLE => 'Student'
+    ];
+
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        if ($panel->getId() === 'student') {
+            return $this->role === self::STUDENT_ROLE;
+        } elseif ($panel->getId() === 'staff') {
+            return $this->role === self::STAFF_ROLE;
+        } elseif ($panel->getId() === 'support') {
+
+            return $this->role === self::SUPPORT;
+        }
+
+        return $this->role === self::ADMIN_ROLE;
+    }
     /**
      * The attributes that are mass assignable.
      *
@@ -21,6 +58,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
+        'gender'
     ];
 
     /**
